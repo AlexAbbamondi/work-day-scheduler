ea689571719c8e2d8711a7683659de897b7fec3e
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-06-29 17:02:20 -0400

    added for loop to loop through hours and apply appropriate class based on what hour is, was, or will be

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-$(document).ready(function() {
-
-//Selects the saveBtn --- adds click event to store the value of what is inside the textarea
-var saveBtn = $(".saveBtn");
-saveBtn.on("click", function() {
+$(document).ready(function () {
+  //Selects the saveBtn --- adds click event to store the value of what is inside the textarea
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", function () {
     var value = $(this).siblings(".description").val();
-    console.log(value);
-})
 
-//Selects the currentDay p tag and sets the date in its place
-var currentDay = $("#currentDay");
-currentDay.text(moment().format("dddd, MMMM Do"));
+    //////////ADD LOCALSTORAGE HERE//////////
+  });
 
+  //Selects the currentDay p tag and sets the date in its place
+  var currentDay = $("#currentDay");
+  var currentDate = moment().format("dddd, MMMM Do");
+  currentDay.text(currentDate);
 
+  //Getting current date and taking just the hours from it
+  var now = new Date(Date.now());
+  var currentHour = now.getHours();
 
+  //Loop through the hours and select the appropriate class based on the id
+  for (var hour = 9; hour < 18; hour++) {
+    var time = "present";
+    if (hour > currentHour) {
+      time = "future";
+    } else if (hour < currentHour) {
+      time = "past";
+    }
 
-
-
-
-})
+    const timeSlot = $("#hour-" + hour);
+    if (timeSlot && timeSlot.length) {
+      timeSlot.children().eq(1).addClass(time);
+    }
+  }
+});
