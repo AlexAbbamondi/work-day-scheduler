a031e25cb5ecd87927d6078cf7bfec9be6fd136c
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-07-01 17:01:44 -0400

    got descriptions to persist through localstorage on page on reload

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-$(document).ready(function () {
-  //Selects the saveBtn --- adds click event to store the value of what is inside the textarea
-  //Set the localstorage key value pair ---sets empty array --pushes values to array -- gets items value and sends it to description
-  var saveBtn = $(".saveBtn");
-  saveBtn.on("click", function () {
-    var value = $(this).siblings(".description").val().trim();
+$(document).ready(() => {
+  //Displays the localstorage key value pair if applicable
+  //for each loop to get key value pair and set it to the textarea with class of description
+  $.each($('.time-block'), (index, item) => {
+    const $item = $(item);
+    const storedValue = localStorage[$item.attr('id')];
+    if (!storedValue || !storedValue.length) {
+      return;
+    }
 
-    localStorage.setItem("key", JSON.stringify(value));
-    var desc = [];
-    desc.push(value);
-    $(".description").value = JSON.parse(localStorage.getItem("key")) || [];
+    const $textArea = $item.find('textarea.description');
+    if (!$textArea.length) {
+      return;
+    }
+
+    $textArea.val(storedValue);
+  });
+
+  //Selects the saveBtn --- adds click event to store the value of what is inside the textarea --- sets key value pair
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", (evt) => {
+    const $saveButton = $(evt.currentTarget);
+    var value = $saveButton.siblings("textarea.description").val().trim();
+    //get the id in this div
+    var hour = $saveButton.parent().attr("id");
+    localStorage.setItem(hour, value);
   });
 
   //Selects the currentDay p tag and sets the date in its place
@@ -31,11 +46,9 @@ $(document).ready(function () {
       time = "past";
     }
 
-    const timeSlot = $("#hour-" + hour);
-    if (timeSlot && timeSlot.length) {
-      timeSlot.children().eq(1).addClass(time);
+    const $timeSlot = $("#hour-" + hour);
+    if($timeSlot && $timeSlot.length) {
+      $timeSlot.children().eq(1).addClass(time);
     }
   }
 });
-
-
